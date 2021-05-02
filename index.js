60c4245e18b669ffbf37a89b351401d6c7b127a5
aplui <alexander.pluimers@gmail.com>
2021-05-02 15:09:05 +0200

    textual clarifications

--- index.js
+++ index.js
@@ -7,43 +7,62 @@ const puppeteer = require('puppeteer');
 (async () => {
     const browser = await puppeteer.launch({headless: true});
     const page = await browser.newPage();
-    const url = 'https://tweakers.net/toetsenborden/aanbod/'
+    const tweakersUrl = 'https://tweakers.net/toetsenborden/aanbod/'
     const accept = 'button[class="ctaButton"]'
 
-        await page.goto(url, { waitUntil: 'networkidle0'  });
+        await page.goto(tweakersUrl, { waitUntil: 'networkidle0'  });
         await page.waitForSelector(accept);
         await page.click(accept);
         await page.waitForNavigation({waitUntil: 'networkidle0'});
 
             let data = await page.evaluate(() => {
 
-            
-            let title = document.querySelector('#listing > table > tbody > tr:nth-child(1) > td:nth-child(2)').innerText;
-            let img = document.querySelector('#listing > table > tbody > tr:nth-child(1) td > a > img').src;
-            let text = document.querySelector('p[class="lead"]').innerText
-            let price = document.querySelector('#listing > table > tbody > tr:nth-child(1) > td.vaprice > p > a').innerText;
-
+                let title = document.querySelector('#listing > table > tbody > tr:nth-child(1) > td:nth-child(2)').innerText;
+                let img = document.querySelector('#listing > table > tbody > tr:nth-child(1) td > a > img').src;
+                let text = document.querySelector('p[class="lead"]').innerText
+                let price = document.querySelector('#listing > table > tbody > tr:nth-child(1) > td.vaprice > p > a').innerText;
+                let url = document.querySelector('#listing > table > tbody > tr:nth-child(1) > td:nth-child(2) > p.title.ellipsis > a').href
+        
         return {
 
             title,
             img,
             text,
-            price
+            price,
+            url
         }
    
     });
 
     console.log(data);
+    const ad = {}
     await browser.close();
   })();
+
+
+  client.on('message', msg => {
+
+    if (msg.content === '!meuk') {
+
+        const embed = new Discord.MessageEmbed()
+        .setTitle(data.title)
+        .setImage(data.img)
+        .setDescription(data.text)
+        .setUrl(data.adurl)
+
+    msg.reply(embed)
+}
+
+})
+
+
+client.login(TOKEN);
 
 client.on('ready', () => {
 
     console.log(`Logged in as ${client.user.tag}!`);
 
 });
-
-client.login(TOKEN);
 
 //timer die om de paar minuten checkt of er wat nieuws is
 // als er iets is gewijzigd tov voorgaande situatie (if else) update doen in discord
